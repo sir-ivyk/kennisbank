113855fb1526874f8c7f74e83bd16a475ca6b3e6
Unknown <m.faro96@gmail.com>
2020-06-08 15:05:19 +0200

    added a view model for filtering documents by tag

---
+++ Models/DocumentTagViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kennisbank.Models
+{
+    public class DocumentTagViewModel
+    {
+        public List<Document> Documents { get; set; }
+        public SelectList Tags { get; set; }
+        public string DocumentTag { get; set; }
+        public string SearchString { get; set; }
+    }
+}
