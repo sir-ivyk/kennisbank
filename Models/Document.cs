10a953feb6cfc9a524dfa19a5cd298afea76b4d1
Unknown <m.faro96@gmail.com>
2020-06-08 10:50:08 +0200

    added a model for documents

---
+++ Models/Document.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kennisbank.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime AddedOn { get; set; }
+        public string Tag { get; set; }
+        public string AddedBy { get; set; }
+        public long FileSize { get; set; }
+    }
+}
