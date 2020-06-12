c21dcc5bdf5996b229e58e9b9deee3fa204cfc48
Unknown <m.faro96@gmail.com>
2020-06-12 19:47:26 +0200

    added the property filepath to the document model

---
+++ Migrations/20200612174632_AddedPropertyFilePathToDocument.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kennisbank.Migrations
+{
+    public partial class AddedPropertyFilePathToDocument : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FilePath",
+                table: "Document",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilePath",
+                table: "Document");
+        }
+    }
+}
